192afea12781eed437e05980159787c11f5fb0df
Ilja Carenko <icarenko@gmail.com>
2022-04-01 10:56:14 +0300

    Init Fork

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -14,9 +14,15 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver","lib/chromedriver.exe");
+        WebDriver chromeDriver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        chromeDriver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+        System.out.println(chromeDriver.getTitle() );
 //         get URL of current page
+        System.out.println(chromeDriver.getCurrentUrl() );
 //         close browser
+        chromeDriver.close();
     }
 }
