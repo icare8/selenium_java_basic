dcd0a1a600203a62b524e524560a5481601de5b9
Ilja Carenko <icarenko@gmail.com>
2022-04-04 16:40:13 +0300

    extraTask #2

--- src/selenium/sample/extra/extra2Task.java
+++ src/selenium/sample/extra/extra2Task.java
@@ -2,6 +2,7 @@ package selenium.sample.extra;
 
 import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,7 +26,11 @@ public class extra2Task {
         driver = new FirefoxDriver();
 //        TODO
 //        go to page https://kristinek.github.io/site/examples/po
+        driver.get("https://kristinek.github.io/site/examples/po");
 //        check the background color of h1 element
+        System.out.println(
+                driver.findElement(By.tagName("h1")).getCssValue("background-color")
+        );
     }
 
     @Test
@@ -34,7 +39,12 @@ public class extra2Task {
         driver = new ChromeDriver();
 //        TODO
 //        go to page https://kristinek.github.io/site/examples/po
+        driver.get("https://kristinek.github.io/site/examples/po");
 //        check the background color of h1 element
+        System.out.println(
+                driver.findElement(By.tagName("h1")).getCssValue("background-color")
+        );
+        Thread.sleep(10000);
     }
 
     @Test
@@ -43,6 +53,12 @@ public class extra2Task {
         driver = new InternetExplorerDriver();
 //        TODO
 //        go to page https://kristinek.github.io/site/examples/po
+        driver.get("https://kristinek.github.io/site/examples/po");
 //        check the background color of h1 element
+        System.out.println(
+                /* IE was 'Unable to find element with tag name == h1 (WARNING: The server did not provide any stacktrace information)' on my machine */
+                driver.findElement(By.tagName("h1")).getCssValue("background-color")
+        );
+        Thread.sleep(10000);
     }
 }
